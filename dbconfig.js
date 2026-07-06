b6ad132da4afe133e39c0e5eb530925f9c981aee
Rehmatullakhan <rehmatullahkhan37@gmail.com>
2026-07-07 03:18:50 +0500

    link

--- dbconfig.js
+++ dbconfig.js
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
 
-const url1 ="mongodb+srv://rehmatullahkhan37_db_user:guruguru@cluster0.mhdz7ht.mongodb.net/?appName=Cluster0"
-const url="mongodb+srv://rehmatullahkhan37_db_user:<db_password>@cluster0.mhdz7ht.mongodb.net/?appName=Cluster0";
+const url1 ="mongodb+srv://rehmatullahkhan37_db_user:Guru26@cluster0.mhdz7ht.mongodb.net/?appName=Cluster0"
+
 const dbName="nodejs";
 export const collectionName="todo";
 const client = new MongoClient(url1)
